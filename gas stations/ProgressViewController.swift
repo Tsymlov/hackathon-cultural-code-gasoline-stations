215c31cdc552e962cc1cc43005a227083cdc4fea
Tsymlov <alexey.tsymlov@gmail.com>
2016-04-23 23:28:34 +0300

    Hide nav bar

---
+++ gas stations/ProgressViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProgressViewController.swift
+//  gas stations
+//
+//  Created by Alexey Tsymlov on 4/23/16.
+//  Copyright © 2016 IPS. All rights reserved.
+//
+
+import UIKit
+
+class ProgressViewController: UIViewController {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+}
