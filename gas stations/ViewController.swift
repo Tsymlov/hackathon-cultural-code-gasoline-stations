e31fbad8339335573d667e4c13b5b87b23f23e99
Tsymlov <alexey.tsymlov@gmail.com>
2016-04-23 15:45:30 +0300

    Initial Commit

---
+++ gas stations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gas stations
+//
+//  Created by Alexey Tsymlov on 4/23/16.
+//  Copyright © 2016 IPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
