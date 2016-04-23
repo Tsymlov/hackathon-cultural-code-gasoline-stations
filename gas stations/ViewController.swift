0ab5acb8fc48378b8a9542af570ba0456f0433a2
Tsymlov <alexey.tsymlov@gmail.com>
2016-04-23 16:16:28 +0300

    Add set up of Navigine core

--- gas stations/ViewController.swift
+++ gas stations/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    private var navigineCore: NavigineCore!{
+        didSet{
+            navigineCore?.delegate = self
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigineCore = NavigineCore.defaultCore()
     }
+}
 
-
+extension ViewController: NavigineCoreDelegate{
+    func didRangePushWithTitle(title: String!, content: String!, image: String!, id: Int) {
+        print(title)
+    }
 }
 
---
+++ gas stations/gas stations-Bridging-Header.h
@@ -0,0 +1,1 @@
+#import "NavigineSDK.h"
