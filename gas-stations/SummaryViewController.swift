cab40d856f242d29ba1eb75173bfd4f7c6e3dc42
Tsymlov <alexey.tsymlov@gmail.com>
2016-04-24 07:28:51 +0300

    Change UI-logic of Summary screen

--- gas-stations/SummaryViewController.swift
+++ gas-stations/SummaryViewController.swift
@@ -9,52 +9,27 @@
 import UIKit
 
 class SummaryViewController: UIViewController {
-    @IBOutlet weak var roublesPickerView: UIPickerView!{
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var valueTextField: UITextField!{
         didSet{
-            roublesPickerView.selectRow(30-1, inComponent: 0, animated: true)
+            valueTextField.becomeFirstResponder()
         }
     }
-    @IBOutlet weak var litresPickerView: UIPickerView!
-}
-
-extension SummaryViewController: UIPickerViewDataSource {
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    enum PickerIDs: String {
-        case Roubles = "Roubles"
-        case Litres = "Litres"
-    }
-    
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        switch pickerView.restorationIdentifier ?? "" {
-        case PickerIDs.Litres.rawValue:
-            return 1000
-        case PickerIDs.Roubles.rawValue:
-            return 15000
-        default:
-            return 0
+    @IBOutlet weak var nextButton: UIButton!{
+        didSet{
+            nextButton.setTitleColor(UIColor.lightGrayColor(), forState: .Disabled)
         }
     }
-}
-
-extension SummaryViewController: UIPickerViewDelegate{
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "\(row + 1)"
+    
+    @IBAction func changeValueOfSegmentedControl() {
+        valueTextField.hidden = segmentedControl.selectedSegmentIndex == SegmentIndexes.FullTank.rawValue
     }
     
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        switch pickerView.restorationIdentifier ?? "" {
-        case PickerIDs.Litres.rawValue:
-            roublesPickerView.selectRow((row+1)*30-1, inComponent: 0, animated: true)
-        case PickerIDs.Roubles.rawValue:
-            let litres = (row+1) / 30
-            let roubles = litres * 30
-            litresPickerView.selectRow(litres-1, inComponent: 0, animated: true)
-            roublesPickerView.selectRow(roubles-1, inComponent: 0, animated: true)
-        default:
-            break
-        }
+    enum SegmentIndexes: Int{
+        case Roubles = 0, Litres, FullTank
+    }
+    
+    @IBAction func changeEditing() {
+                nextButton.enabled = (!(valueTextField.text?.isEmpty ?? true)) && (Int(valueTextField.text ?? "") ?? 0) != 0
     }
 }
